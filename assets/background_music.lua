4be459a35cb7c30605dd84fb8849f06836da2566
timerunner16 <calvin@koose.com>
2025-06-25 22:16:26 -0400

    key to mute background music

--- assets/background_music.lua
+++ assets/background_music.lua
@@ -1,10 +1,29 @@
 -- object references
 local sound
+local pdata
+local input
+
+function init()
+	pdata = Engine.PDataManager
+	input = Engine.InputManager
+end
 
 function process(delta)
 	if (not sound) then
 		sound = Engine.Workspace:CreateAudioInstance("BACKGROUND_MUSIC", "BACKGRND", nil)
 		sound.Looping = true
-		sound:Play()
+		if (not pdata:GetValue("bgm_muted")) then
+			sound:Play()
+		end
+	end
+
+	local switch = input:QueryKey(Keys.M)
+	if (switch.Pressed and switch.OnEdge) then
+		pdata:SetValue("bgm_muted", not pdata:GetValue("bgm_muted"))
+		if (pdata:GetValue("bgm_muted")) then
+			sound:Pause()
+		else
+			sound:Play()
+		end
 	end
 end
